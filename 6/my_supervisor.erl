e4e2d9854ff36a421bea8641257389a8f083c8ea
Francis Wang <francis@bposolutions.com>
2015-10-27 17:30:28 -0700

    Complete chapter 6

--- 6/my_supervisor.erl
+++ 6/my_supervisor.erl
@@ -7,7 +7,7 @@
 %%   (c) Francesco Cesarini and Simon Thompson
 
 -module(my_supervisor).
--export([start_link/2, stop/1, info/1]).
+-export([start_link/2, stop/1, info/1, start_child/2, stop_child/2]).
 -export([init/1]).
 
 start_link(Name, ChildSpecList) ->
@@ -21,7 +21,8 @@ start_children([]) -> [];
 start_children([{M, F, A, Type} | ChildSpecList]) ->
     case (catch apply(M,F,A)) of
         {ok, Pid} ->
-            [{Pid, {M,F,A,Type,[]}}|start_children(ChildSpecList)];
+            % use now() as unique id to identify a child
+            [{Pid, {M,F,A,Type,[]}, now()}|start_children(ChildSpecList)];
         _ ->
             start_children(ChildSpecList)
     end.
@@ -31,7 +32,7 @@ start_children([{M, F, A, Type} | ChildSpecList]) ->
 %% child, replacing its entry in the list of children stored in the ChildList variable:
 
 restart_child(Pid, ChildList) ->
-    {value, {Pid, {M,F,A,Type,RestartTs}}} = lists:keysearch(Pid, 1, ChildList),
+    {value, {Pid, {M,F,A,Type,RestartTs}, Id}} = lists:keysearch(Pid, 1, ChildList),
     NewChildList = lists:keydelete(Pid,1,ChildList),
     case Type of
          transient -> NewChildList;
@@ -43,7 +44,7 @@ restart_child(Pid, ChildList) ->
                     NewChildList;
                 true ->
                     {ok, NewPid} = apply(M,F,A),
-                    [{NewPid, {M,F,A,Type,[ts()|NewRestartTs]}}|NewChildList]
+                    [{NewPid, {M,F,A,Type,[ts()|NewRestartTs]}, Id}|NewChildList]
             end
     end.
 
@@ -62,7 +63,28 @@ loop(ChildList) ->
             From ! {reply, terminate(ChildList)};
         {info, From} ->
             From ! {reply, ChildList},
-            loop(ChildList)
+            loop(ChildList);
+        {start_child, From, ChildSpec} ->
+            case start_child(ChildSpec) of
+                {Pid, Spec, Id} ->
+                    NewChildList = [{Pid, Spec, Id}|ChildList],
+                    From ! {reply, Id},
+                    loop(NewChildList);
+                {error, cannot_start_child} ->
+                    From ! {reply, error},
+                    loop(ChildList)
+            end;
+        {stop_child, From, Id} ->
+            case lists:keysearch(Id, 3, ChildList) of
+                {value, {Pid, _Spec, _Id}} -> 
+                    unlink(Pid),
+                    exit(Pid, kill),
+                    From ! {reply, ok},
+                    loop(lists:keydelete(Id, 3, ChildList));
+                false -> 
+                    From ! {reply, error},
+                    loop(ChildList)
+            end
     end.
 
 %% We stop the supervisor by calling the synchronous client function stop/0. Upon receiving the 
@@ -82,3 +104,18 @@ terminate(_ChildList) -> ok.
 info(Name) ->
     Name ! {info, self()},
     receive {reply, Reply} -> Reply end.
+
+start_child(Name, ChildSpec) ->
+    Name ! {start_child, self(), ChildSpec},
+    receive {reply, Reply} -> Reply end.
+
+start_child({M,F,A,Type}) ->
+    case (catch apply(M,F,A)) of
+        {ok, Pid} -> {Pid, {M,F,A,Type,[]}, now()};
+        _ -> {error, cannot_start_child}
+    end.
+
+stop_child(Name, Id) ->
+    Name ! {stop_child, self(), Id},
+    receive {reply, Reply} -> Reply end.
+
