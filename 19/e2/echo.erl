c3f3fa3ad9cec45be7a8bf08843e23b8d21bb1dd
Francis Wang <go717franciswang@hotmail.com>
2015-11-13 00:03:25 -0800

    19-2

--- 19/e2/echo.erl
+++ 19/e2/echo.erl
@@ -30,21 +30,10 @@ loop() ->
     end.
 
 echo_test() ->
-    eunit:test(
-        {spawn,
-            {setup,
-                fun() -> 
-                        Pid = spawn(echo, loop, []),
-                        Pid ! {self(), hello},
-                        Pid
-                end,
-                fun(Pid) -> Pid ! stop end,
-                fun() -> 
-                        receive
-                            Reply -> ?assertMatch({_Pid, hello1}, Reply)
-                        end,
-                        ?assertEqual(1,2)
-                end
-            }
-        }).
+    Pid = spawn(echo, loop, []),
+    Pid ! {self(), hello},
+    receive
+        Reply -> ?assertMatch({_Pid, hello}, Reply)
+    end,
+    Pid ! stop.
 
